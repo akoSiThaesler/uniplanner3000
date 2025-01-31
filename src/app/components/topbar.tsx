612b0d57173c9d0c286ae6bb3d95df14c8d8c167
Tom Weise <twe@eschbach.com>
2025-01-31 11:22:30 +0100

    added card

--- src/app/components/topbar.tsx
+++ src/app/components/topbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./topbar.module.css";
 import AutoStoriesSharpIcon from "@mui/icons-material/AutoStoriesSharp";
+import Image from "next/image";
 
 type NavbarProps = {
   children?: React.ReactNode;
@@ -11,27 +12,27 @@ Temporary based on Figma design !! Bound to change based on feedback :D
 Main thing here is that when the user clicks on the Icon it calls onMenuClick which toggles the sidebar
 I.e
     A[User clicks AutoStoriesSharpIcon in Topbar] -->|triggers| B[onMenuClick prop in Topbar]
-    B -->|executes| C[toggleSidebar in page.tsx]
-    C -->|updates| D[isSidebarOpen state]
-    D -->|passes to| E[Sidebar component isOpen prop]
-    D -->|passes to| F[Main component isOpen prop]
-    D -->|passes to| G[Footer component isOpen prop]
-    E -->|updates| H[Sidebar visibility]
-    F -->|adjusts| I[Main content margin]
-    G -->|adjusts| J[Footer margin]
+    B -->| executes  | C[toggleSidebar in page.tsx]
+    C -->| updates   | D[isSidebarOpen state]
+    D -->| passes to | E[Sidebar component isOpen prop]
+    D -->| passes to | F[Main component isOpen prop]
+    D -->| passes to | G[Footer component isOpen prop]
+    E -->| updates   | H[Sidebar visibility]
+    F -->| adjusts   | I[Main content margin]
+    G -->| adjusts   | J[Footer margin]
     :D
 */
 
-const NavBar: React.FC<NavbarProps> = ({ children, onMenuClick }) => {
+const NavBar: React.FC<NavbarProps> = ({ children }) => {
   return (
     <nav className={styles.nav}>
       <div className={styles.titleRow}>
-        <AutoStoriesSharpIcon
-          width="24px"
-          height="24px"
-          onClick={onMenuClick}
-          style={{ cursor: "pointer" }}
-        />
+        <Image
+          width={50}
+          height={50}
+          src=".../public/logo.svg"
+          alt="Logo"
+        ></Image>
         <h1 className={styles.title}>Uniplanner</h1>
       </div>
       <ul className={styles.linkList}>
